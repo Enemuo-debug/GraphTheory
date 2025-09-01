abc954a59ac81a273f2819f976736ebd4722b844
Enemuo-debug <chimzuruoke2017@gmail.com>
2025-09-01 23:58:40 +0100

    Priority Queue Implementation Complete

--- Heap.cs
+++ Heap.cs
@@ -7,20 +7,59 @@ namespace GraphTheory
 {
     public class Heap
     {
-        private List<int> _heap;
+        public List<int> _heap;
         public Heap()
         {
             _heap = [];
         }
         public void Add(int x)
         {
-            
+            _heap.Add(x);
+            FixHeapFromBottom(_heap.Count - 1);
+        }
+        public int? Peek()
+        {
+            // Get the minimum element without removing it
+            if (_heap.Count == 0) return null;
+            return _heap[0];
+        }
+        public int? Pop()
+        {
+            if (_heap.Count == 0) return null;
+            int output = _heap[0];
+            _heap[0] = _heap[_heap.Count - 1];
+            _heap.RemoveAt(_heap.Count - 1);
+            FixHeapFromTop();
+            return output;
+        }
+        public void FixHeapFromTop(int index = 0)
+        {
+            int leftChildIndex = 2 * index + 1;
+            int rightChildIndex = 2 * index + 2;
+            int smallest = index;
+            if (leftChildIndex < _heap.Count && _heap[leftChildIndex] < _heap[smallest]) smallest = leftChildIndex;
+            if (rightChildIndex < _heap.Count && _heap[rightChildIndex] < _heap[smallest]) smallest = rightChildIndex;
+            if (smallest != index)
+            {
+                Swap(index, smallest);
+                FixHeapFromTop(smallest);
+            }
         }
         public void Swap(int a, int b)
         {
-            a = a + b;
-            b = a - b;
-            a = a - b;
+            _heap[a] += _heap[b];
+            _heap[b] = _heap[a] - _heap[b];
+            _heap[a] -= _heap[b];
+        }
+        public void FixHeapFromBottom(int index)
+        {
+            if (index <= 0 || index >= _heap.Count) return;
+            int parentIndex = (index - 1) / 2;
+            if (_heap[index] < _heap[parentIndex])
+            {
+                Swap(index, parentIndex);
+                FixHeapFromBottom(parentIndex);
+            }
         }
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using GraphTheory;
+using static GraphTheory.Heap;
 
 class Program
 {
@@ -14,25 +15,40 @@ class Program
         }
 
         // Add links (edges) with weights (costs)
-        graph.AddLink(1, 2, 5);
-        graph.AddLink(1, 3, 2);
-        graph.AddLink(1, 4, 1);
+        // graph.AddLink(1, 2, 5);
+        // graph.AddLink(1, 3, 2);
+        // graph.AddLink(1, 4, 1);
 
-        graph.AddLink(2, 5, 3);
-        graph.AddLink(2, 6, 6);
+        // graph.AddLink(2, 5, 3);
+        // graph.AddLink(2, 6, 6);
 
-        graph.AddLink(3, 7, 4);
-        graph.AddLink(3, 8, 8);
+        // graph.AddLink(3, 7, 4);
+        // graph.AddLink(3, 8, 8);
 
-        graph.AddLink(4, 9, 7);
-        graph.AddLink(4, 10, 9);
+        // graph.AddLink(4, 9, 7);
+        // graph.AddLink(4, 10, 9);
 
-        graph.AddLink(5, 6, 2);
-        graph.AddLink(6, 7, 1);
-        graph.AddLink(8, 9, 3);
-        graph.AddLink(9, 10, 2);
+        // graph.AddLink(5, 6, 2);
+        // graph.AddLink(6, 7, 1);
+        // graph.AddLink(8, 9, 3);
+        // graph.AddLink(9, 10, 2);
 
-        Console.WriteLine("DFS starting from node 1:");
-        graph.BFS(1);
+        // Console.WriteLine("DFS starting from node 1:");
+        // graph.BFS(1);
+        Heap heap = new Heap();
+        heap.Add(15);
+        heap.Add(9);
+        heap.Add(80);
+        heap.Add(10);
+        heap.Add(4);
+        heap.Add(5);
+        heap.Add(25);
+        heap.Add(19);
+        heap.Add(8);
+        heap.Add(1);
+        for (int i = 0; i < 10; i++)
+        {
+            Console.WriteLine(heap.Pop());
+        }
     }
 }
